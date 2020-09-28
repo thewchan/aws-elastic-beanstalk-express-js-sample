5c138cb036ce0cfac6d32ae04f7488ff7a2d31dd
Matt Chan <thewchan@gmail.com>
2020-09-28 01:15:52 -0700

    Added approval stage

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! It is me again!'));
+app.get('/', (req, res) => res.send('Hello World! This update required a manual review!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
